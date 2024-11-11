85acb69603c17c064f82bbf90242921bdc1edf50
tajwarsaiyeed <rjabid36@gmail.com>
2024-11-11 06:27:14 +0600

    Add initial app structure with models, views, templates, and routing

---
+++ djangoProjectPracticeDay2/first_app/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+
+def first_app_home(req):
+   meals = [
+        {
+            "strMeal": "BeaverTails",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+            "idMeal": "52928",
+            "strDescription": "A fried dough pastry, individually hand stretched to resemble a beaver’s tail."
+        },
+        {
+            "strMeal": "Breakfast Potatoes",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+            "idMeal": "52965",
+            "strDescription": "Crispy and golden breakfast potatoes, perfect for a hearty morning meal."
+        },
+        {
+            "strMeal": "Canadian Butter Tarts",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+            "idMeal": "52923",
+            "strDescription": "A classic Canadian dessert with a sweet and gooey filling."
+        },
+        {
+            "strMeal": "Montreal Smoked Meat",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+            "idMeal": "52927",
+            "strDescription": "A type of kosher-style deli meat product made by salting and curing beef brisket."
+        },
+        {
+            "strMeal": "Nanaimo Bars",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+            "idMeal": "52924",
+            "strDescription": "A layered dessert bar with a crumbly base, custard-flavored middle, and chocolate topping."
+        },
+        {
+            "strMeal": "Pate Chinois",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+            "idMeal": "52930",
+            "strDescription": "A French Canadian dish similar to shepherd's pie, made with ground beef, corn, and mashed potatoes."
+        },
+        {
+            "strMeal": "Pouding chomeur",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+            "idMeal": "52932",
+            "strDescription": "A traditional Quebec dessert made with cake batter and hot syrup."
+        },
+        {
+            "strMeal": "Poutine",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+            "idMeal": "52804",
+            "strDescription": "A Canadian dish consisting of fries topped with cheese curds and gravy."
+        },
+        {
+            "strMeal": "Rappie Pie",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+            "idMeal": "52933",
+            "strDescription": "A traditional Acadian dish made with grated potatoes and meat."
+        },
+        {
+            "strMeal": "Split Pea Soup",
+            "strMealThumb": "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+            "idMeal": "52925",
+            "strDescription": "A hearty soup made from dried peas, often with ham or bacon."
+        }
+    ]
+   return render(req, 'home.html', {"meals": meals})

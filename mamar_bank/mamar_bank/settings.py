9342a5364d144898b69d204c3729774e6e763439
tajwarsaiyeed <rjabid36@gmail.com>
2025-01-17 13:06:52 +0600

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for production environment

--- mamar_bank/mamar_bank/settings.py
+++ mamar_bank/mamar_bank/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=["*"]
+ALLOWED_HOSTS = ["mamar-bank-xpcd.onrender.com"]
 CSRF_TRUSTED_ORIGINS=["https://mamar-bank-xpcd.onrender.com"]
 
 

7b59430d1e90dc75c05f401bcc7ad69cd5e476d8
tajwarsaiyeed <rjabid36@gmail.com>
2025-01-30 16:57:47 +0600

    feat: Update settings to allow all hosts and add requirements file

--- drf/smart_care/smart_care/settings.py
+++ drf/smart_care/smart_care/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -66,20 +66,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'smart_care.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {

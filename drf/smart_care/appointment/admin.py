eac45c064314ec9fc1f3fa79e5cbbe77df7c9469
tajwarsaiyeed <rjabid36@gmail.com>
2025-01-30 07:44:39 +0600

    feat: Add appointment reminder email functionality and HTML template feat: Protect APIs feat: Added Pagination and filtering

--- drf/smart_care/appointment/admin.py
+++ drf/smart_care/appointment/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 from . import models
 
 class AppointmentAdmin(admin.ModelAdmin):
@@ -8,5 +11,23 @@ class AppointmentAdmin(admin.ModelAdmin):
 
     def doctor_name(self,obj):
         return f"Dr. {obj.doctor.user.first_name}"
+
+    def save_model(self, request, obj, form, change):
+        if obj.appointment_status == 'Running' and obj.appointment_types == "Online":
+            email_subject = "Appointment Reminder"
+            email_body = render_to_string('appointment_reminder.html', {
+                'patient_name': obj.patient.user.first_name,
+                'doctor_name': obj.doctor.user.first_name,
+                'symptom': obj.symptom,
+                'time': obj.time,
+                'meet_link': obj.doctor.meet_link
+            })
+            email = EmailMultiAlternatives(
+                email_subject,
+                '',
+                to=[obj.patient.user.email]
+            )
+            email.attach_alternative(email_body, "text/html")
+            email.send()
 
 admin.site.register(models.Appointment, AppointmentAdmin)
--- drf/smart_care/doctor/views.py
+++ drf/smart_care/doctor/views.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 from .models import Doctor, Specialization, Designation, Review, AvailableTime
 from .serializers import DoctorSerializer, SpecializationSerializer, DesignationSerializer, AvailableTimeSerializer, \
     ReviewSerializer
+from rest_framework import pagination, filters
 
+
+class DoctorPagination(pagination.PageNumberPagination):
+    page_size = 1
+    page_size_query_param = page_size
+    max_page_size = 100
 
 class DoctorViewset(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    pagination_class = DoctorPagination
 
 
 class SpecializationViewset(viewsets.ModelViewSet):
@@ -19,11 +28,20 @@ class DesignationViewset(viewsets.ModelViewSet):
     serializer_class = DesignationSerializer
 
 
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        doctor_id = request.query_params.get('doctor_id')
+        if doctor_id:
+            return queryset.filter(doctor=doctor_id)
+        return queryset
+
 class AvailableTimeViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = AvailableTime.objects.all()
     serializer_class = AvailableTimeSerializer
-
+    filter_backends = [AvailableTimeForSpecificDoctor]
 
 class ReviewViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
